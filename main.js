4889e3058af02afedd5d3c7e1696b2efa30376cb
Ralfi18 <ralidimitrovdimitrov@gmail.com>
2017-04-06 16:13:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+﻿$(document).ready(function(){
+	var wrapper = $('div.wrapper'), 	
+	    btn_close = wrapper.find('button.btn-close'), 	
+	    btn_open = wrapper.find('button.btn-open'), 
+	    content = wrapper.find('section.content'),
+	    content_p = wrapper.find('section.content p'),
+	    out,	/* content */
+	    len,
+	    i = 0;			    
+
+	/*
+	========================================================
+	Remove data  
+	========================================================
+  	*/
+  	
+  	btn_close.on('click', function(e){
+  		$('p').remove();
+  		i = 0;
+  	});
+
+	/*
+	========================================================
+	Load data  
+	========================================================
+  	*/
+	btn_open.on('click', function(e){
+	var url_p = "pdo_data.php" /* url*/
+    	$.getJSON( url_p, function(result){
+    		/*$("p").remove();*/
+    		/*
+        	$.each(result, function(i, field){
+        			out = display_text(field);
+            			 content.append("<p>"+field.name + "</p>"); 
+            			content.append(out);
+        		});
+        	*/
+        	
+        	len = result.length;
+			while ( i < len )
+			{
+				r = result[i];
+        			out = display_text( r );
+            			/*content.append("<p>"+field.name + "</p>"); */
+            			content.append(out);
+            			i++;
+			}
+    		});
+    		    		    		
+    	/*  turn the event of after exec. to prevent bubuling */	
+    	/*$( this ).off( e );*/
+	});
+	
+	function display_text( data, ht )
+	{
+		if ( ht == null )
+		{
+			ht = "p";
+		}
+		
+		return  "<"+ht+">" + 'My name is ' + data.name + " and i'm " + data.age + " years old." + "</"+ht+">";
+	}	
+});
---
+++ pdo_data.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+$data = [];
+
+$dbh = new PDO('mysql:host=localhost;dbname=dbname', "user", "pass");
+$sql = $dbh->query('SELECT * FROM data'); 	
+
+$result = $sql->fetchAll();
+
+echo json_encode($result );
