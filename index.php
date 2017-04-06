73a90104a80947e855c8da79194a08e2bb9d10b2
Ralfi18 <ralidimitrovdimitrov@gmail.com>
2017-04-06 16:04:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<!DOCTYPE html><html lang="en"><head>  <meta charset="UTF-8" />  <title>Read JSON parsed using PHP</title>  <link rel="stylesheet" href="" />   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> </head><body> <div class='wrapper'>  <button class="btn-open" >Open on click</button>  <button class="btn-close" >Close on click</button>  <section class='content'></section> </div><script src='main.js'></script></body></html>
+<!DOCTYPE html><html lang="en">
+  <head>  
+    <meta charset="UTF-8" />
+    <title>Read JSON parsed using PHP</title>  
+    <link rel="stylesheet" href="" />   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+  </head>
+  <body> 
+    <div class='wrapper'>  
+      <button class="btn-open" >Open on click</button>  
+      <button class="btn-close" >Close on click</button>  
+      <section class='content'></section> 
+    </div>
+    <script src='main.js'></script>
+  </body>
+</html>
